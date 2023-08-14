7433cf735ed57b107bf2b0b2a8907816cc343935
Dillon Armstrong <dillon@Dillons-MacBook-Pro.local>
2023-08-14 16:02:04 -0600

    basic server setup and dotenv. added nodemon to dev deps

--- server/app.js
+++ server/app.js
@@ -1,3 +1,22 @@
+/* eslint-disable no-console */
 require('dotenv').congig();
+const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// comment out for production
+app.use(morgan());
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
