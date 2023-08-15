4500cbc9da3eb593557961e57a0f0afd9883d017
Dillon Armstrong <dillon@Dillons-MacBook-Pro.local>
2023-08-15 13:40:57 -0600

    update gitignore. add gsap. first animation.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import gsap from 'gsap';
 
 export default function App() {
+  React.useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.from(".name", {
+        x: 100,
+        duration: 2,
+        repeat: 2,
+      });
+    });
+    return () => ctx.revert();
+  });
+
   return (
-    <h1>Dillon Armstrong</h1>
+    <h1 className="name">Dillon Armstrong</h1>
   );
 }
