791b16a39c1efdaaa471b9dd9cefc6433b106869
Dillon Armstrong <dillon@Dillons-MacBook-Pro.local>
2023-08-29 13:40:09 -0600

    working on testing

--- spec/app.spec.jsx
+++ spec/app.spec.jsx
@@ -1,3 +1,22 @@
 import {
   render, screen, fireEvent, cleanup,
 } from '@testing-library/react';
+import {
+  expect, describe, test, beforeEach, afterEach,
+} from 'jest';
+
+import React from 'react';
+import App from '../client/src/components/App';
+
+describe('App', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  afterEach(cleanup);
+
+  test('Should display candidates name', async () => {
+    const header = await screen.getByDisplayValue('Dillon Armstrong');
+    expect(header).toExist();
+  });
+});
