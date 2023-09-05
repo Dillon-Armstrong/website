068092a77c6cf30e40514c7d905f09526abf92dc
Dillon Armstrong <dillon@Dillons-MacBook-Pro.local>
2023-09-05 10:11:24 -0600

    babelrc and jsdom dep

--- client/src/components/skillsList.jsx
+++ client/src/components/skillsList.jsx
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 import Skill from './skill';
 
 export default function SkillsList() {
-  const [skills, setSkills] = useState({
+  const [skills] = useState({
     Languages: ['Javascript', 'Typescript'],
     Environment: ['Node.js'],
     Libraries: ['React/React-Native', 'Axios', 'GSAP'],
     Frameworks: ['Express'],
     Testing: ['Jest', 'React-testing-library'],
     Databases: ['PostgreSQL', 'MongoDB', 'MySQL'],
+    DevTools: ['Postman', 'K6', 'Loader.io'],
+    Deployment: ['AWS', 'Nginx', 'New relic', 'htop'],
   });
 
   const names = Object.keys(skills);
